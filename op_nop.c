d08245284f7a627976cc878ef102c6c81f02912b
piero2023 <estibensmax2022@gmail.com>
2022-05-12 10:28:08 -0700

    implementing the nop function

---
+++ op_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * func_nop - Funcion to Implement the nop opcode.
+ * @stack: list
+ * @line_number: line number of file read
+ */
+
+void func_nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
