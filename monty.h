4378fb1c34d0e2549bbb2ccf298c7b49435361f9
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-11 19:15:04 -0500

    Removing conflicts from github

--- monty.h
+++ monty.h
@@ -1,17 +1,12 @@
 #ifndef _MONTY_H_
 #define _MONTY_H_
 
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdlib.h>
-=======
 /* libraries*/
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <ctype.h>
->>>>>>> piero2023
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -23,15 +18,9 @@
  */
 typedef struct stack_s
 {
-<<<<<<< HEAD
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
-=======
 	int n;
 	struct stack_s *prev;
 	struct stack_s *next;
->>>>>>> piero2023
 } stack_t;
 
 /**
@@ -44,13 +33,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-<<<<<<< HEAD
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-=======
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 /*Global variables*/
 extern FILE *fl;
@@ -63,6 +47,5 @@ int get_operator(char *op, stack_t **stack, unsigned int line_number);
 void func_pall(stack_t **stack, unsigned int line_number);
 void func_push(char *token, stack_t **stack, unsigned int line_number);
 void _free(stack_t *stack, char *line);
->>>>>>> piero2023
 
 #endif /* _MONTY_H_*/
