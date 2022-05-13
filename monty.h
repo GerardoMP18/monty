f42e8b9967d2773676d899e0993041c3842b6a52
piero2023 <estibensmax2022@gmail.com>
2022-05-12 19:28:58 -0700

    adding the prototype of the div function

--- monty.h
+++ monty.h
@@ -52,6 +52,7 @@ void func_swap(stack_t **stack, unsigned int line_number);
 void func_add(stack_t **stack, unsigned int line_number);
 void func_nop(stack_t **stack, unsigned int line_number);
 void func_sub(stack_t **stack, unsigned int line_number);
+void func_div(stack_t **stack, unsigned int line_number);
 void _free(stack_t *stack, char *line);
 
 #endif /* _MONTY_H_*/
